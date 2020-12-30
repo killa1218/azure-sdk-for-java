c0bccd34d6942513d096118a0088a6ce524afcc3
killa <jaytin1218@hotmail.com>
2020-12-30 09:40:50 +0800

    [mod] Change data folder to user home.

--- sdk/resourcemanager/azure-resourcemanager-test/src/main/java/com/azure/resourcemanager/test/Constants.java
+++ sdk/resourcemanager/azure-resourcemanager-test/src/main/java/com/azure/resourcemanager/test/Constants.java
@@ -4,5 +4,5 @@ package com.azure.resourcemanager.test;
  * Constants
  */
 public class Constants {
-    public static final String dataDir = "~/json2java/data/raw";
+    public static final String dataDir = String.format("%s/%s", System.getProperty("user.home"), "json2java/data/raw");
 }
